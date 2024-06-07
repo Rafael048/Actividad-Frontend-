0941b287941b2c1ab6d192906e55476318441c0f
Rafael <rafael2005perezmarin@gmail.com>
2024-06-07 16:41:54 -0400

    Creados los archivos de controladores y modelos para usuarios, agregadas rutas provicionales para la prueba de los metodos encargados de la manipulacion de datos, agregada la conexion a la base de datos, asi mismo como el archivo para insertarla

---
+++ connection.js
@@ -0,0 +1,10 @@
+const e = require('express')
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+    host: process.env.BD_HOST,
+    user : process.env.BD_USER,
+    password: process.env.BD_PASSWORD,
+    database: process.env.BD_NAME
+})
+connection.connect()
+module.exports = connection
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const AutenticationControllers = require("../Controllers/AutenticationControllers")
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/login",function(req,res,next){
+  res.render("login")
+})
+router.get("register", function(req,res, next){
+  res.render("register")
+})
+router.post("register",function(req,res,next){
+  AutenticationControllers.register(req.body)
+  .then((results)=>{
+    res.send(results)
 
+  })
+  .catch((e)=>{
+    res.send(e)
+  })
+})
 module.exports = router;
